01176462c418598c6d6d6b81a3371ac6b8ff3b9d
BNK <balank244@gmail.com>
2020-07-29 20:59:40 +0530

    Commit

--- js/script.js
+++ js/script.js
@@ -63,8 +63,20 @@ function showSection(element)
     const target = element.getAttribute('href').split('#')[1];
     document.querySelector("#"+target).classList.add("active")
 
+}
 
+const navTogglerBtn = document.querySelector('.nav-toggler'),
+    aside = document.querySelector('.side-nav');
 
+navTogglerBtn.addEventListener('click', asideSectionTogglerBtn);
+
+function asideSectionTogglerBtn() 
+{
+    aside.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle('open');
+    }
 }
 
 // projects
