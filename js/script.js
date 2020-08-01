8fa10bc692ebc24835a3d749fb0c0f5c8597a8fd
BNK <balank244@gmail.com>
2020-08-01 14:46:13 +0530

    responsive

--- js/script.js
+++ js/script.js
@@ -112,3 +112,24 @@ const filterContainer = document.querySelector('.projects-filter'),
     }
 
 
+
+////////////////////////// style switcher ////////////////////////////////////
+
+
+const links = document.querySelectorAll('.alternate-style'),
+    totalLinks = links.length;
+
+function setActiveStyle(color)
+{
+    for (let i = 0; i < totalLinks; i++) {
+        if (color === links[i].getAttribute('title')) {
+            links[i].removeAttribute('disabled');
+        } else {
+            links[i].setAttribute('disabled', true);
+        }
+    }
+}
+
+document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
+    document.querySelector('.style-switcher').classList.toggle('open');
+});
